6effa26ac80f0a1580729a300306c4c6b28ba52f
alimsx <81366596+Whyriez@users.noreply.github.com>
2023-06-25 15:55:42 +0800

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -5,12 +5,18 @@ import { Sequelize } from "sequelize";
 //   dialect: "mysql",
 // });
 
+// database: secret-chat
+// username: ynwmoonzqr6hqfxp020b
+// host: aws.connect.psdb.cloud
+// password: pscale_pw_cEAUOs6N3bUvIe2bSEaWBCW9o4GwGiJHmiPDJAKe6lj
+
+
 const db = new Sequelize(
   "secret-chat",
-  "a9627n062go85cg42n92",
-  "pscale_pw_LIgY5s7TezAAbfmqQRHX59OwrCgdEDy56fNTD9LPS6C",
+  "ynwmoonzqr6hqfxp020b",
+  "pscale_pw_cEAUOs6N3bUvIe2bSEaWBCW9o4GwGiJHmiPDJAKe6lj",
   {
-    host: "ap-southeast.connect.psdb.cloud",
+    host: "aws.connect.psdb.cloud",
     dialect: "mysql",
     dialectOptions: {
       ssl: {
