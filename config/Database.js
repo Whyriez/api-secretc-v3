410f9f06431038516f628dd45d72f85e476ae6e3
alimsx <81366596+Whyriez@users.noreply.github.com>
2023-06-25 16:16:25 +0800

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -13,10 +13,10 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize(
   "secret-chat",
-  "rneua8cjsj490cnqv7ho",
-  "pscale_pw_T7D2ZG5hNc2o8XLVTjHQMIWhoJ52h0Wq9JjrK81BAXQ",
+  process.env.USERNAME,
+  process.env.PASSWORD,
   {
-    host: "aws.connect.psdb.cloud",
+    host: process.env.HOST,
     dialect: "mysql",
     dialectOptions: {
       ssl: {
